3f29b603057b2dcd7339d8ab8f9055e5b75e2842
Khubuda <McHuu2003@gmail.com>
2024-05-15 09:54:13 +0800

    Edit

--- furniture website Tusul/category.php
+++ furniture website Tusul/category.php
@@ -142,39 +142,8 @@
         </section>
         
     </div>
-
-    <section class="footer" id="footer">
-        <div class="footer-box">
-            <h2>Urban <span>Aero.</span></h2>
-            <p>Таны хэрэгцээнд төгс нийцэх өөр өөрийн өнгө аяс бүхий дэлхийн шилдэг брэндүүдийг таны гэрт.</p>
-            <div class="social">
-                <a href="#"><i class='bx bxl-facebook'></i></a>
-                <a href="#"><i class='bx bxl-twitter'></i></a>
-                <a href="#"><i class='bx bxl-instagram'></i></a>
-            </div>
-        </div>
-        
-        <div class="footer-box contact-info">
-            <h3>Холбоо барих</h3>
-            <span>Монгол улс, Улаанбаатар хот, Хан-Уул дүүрэг, Чингисийн өргөн чөлөө, Мишээл экспо, Мишээл-3 төв</span>
-            <span>Утас: 7777-9393</span>
-            <span>info@UrbanAero.com</span>
-        </div>
-    </section>
-    <div class="copyright">
-        <p>&#169; "UrbanAero" ХХК ©2024</p>
-    </div>
+    <?php include 'footer.php'?>;
     <script src="main.js">
-        function filterProducts(categoryId) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("product-container").innerHTML = this.responseText;
-            }
-        };
-        xhttp.open("GET", "filterProducts.php?category_id=" + categoryId, true);
-        xhttp.send();
-    }
     </script>
 </body>
 </html>
--- furniture website Tusul/index.php
+++ furniture website Tusul/index.php
@@ -90,27 +90,7 @@
     </div>
 </section>
  <!-- Footer -->
- <section class="footer" id="footer">
-    <div class="footer-box">
-        <h2>Urban <span>Aero.</span></h2>
-        <p>Таны хэрэгцээнд төгс нийцэх өөр өөрийн өнгө аяс бүхий дэлхийн шилдэг брэндүүдийг таны гэрт.</p>
-        <div class="social">
-            <a href="#"><i class='bx bxl-facebook'></i></a>
-            <a href="#"><i class='bx bxl-twitter'></i></a>
-            <a href="#"><i class='bx bxl-instagram'></i></a>
-        </div>
-    </div>
-    
-    <div class="footer-box contact-info">
-        <h3>Холбоо барих</h3>
-        <span>Монгол улс, Улаанбаатар хот, Хан-Уул дүүрэг, Чингисийн өргөн чөлөө, Мишээл экспо, Мишээл-3 төв</span>
-        <span>Утас: 7777-9393</span>
-        <span>info@UrbanAero.com</span>
-    </div>
-</section>
-<div class="copyright">
-    <p>&#169; "UrbanAero" ХХК ©2024</p>
-</div>
+ <?php include 'footer.php'?>;
 
 <div class="popup-container" id="popup-container">
     <div class="popup-content">
